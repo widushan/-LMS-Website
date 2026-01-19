0f97e43797791d4329102e27871ea33f3b3e3bde
widushan <114930601+widushan@users.noreply.github.com>
2026-01-20 00:14:39 +0530

    course commit
    Building Add Course Backend section

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,6 @@
 import { clerkClient } from '@clerk/express'
+import Course from '../models/Course.js'
+import { v2 as cloudinary } from 'cloudinary'
 
 // update role to educator
 export const updateRoleToEducator = async (req, res)=>{
@@ -13,6 +15,44 @@ export const updateRoleToEducator = async (req, res)=>{
         
         res.json({ success: true, message: 'You can publish a course now' })
         
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// Add New Course
+export const addCourse = async (req, res)=>{
+    try {
+        const { courseData } = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+        
+        if(!imageFile){
+            return res.json({ success: false, message: 'Thumbnail Not Attached' })
+        }
+        
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+
+        res.json({ success: true, message: 'Course Added' })
+        
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// Get Educator Courses
+export const getEducatorCourses = async (req, res)=>{
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+        res.json({ success: true, courses })
     } catch (error) {
         res.json({ success: false, message: error.message })
     }
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import {updateRoleToEducator} from '../controllers/educatorController.js';
+import {addCourse, getEducatorCourses, updateRoleToEducator} from '../controllers/educatorController.js';
+import upload from '../configs/multer.js';
+import { protectEducator } from '../middlewares/authMiddleware.js';
 
 const educatorRouter = express.Router()
 
 // Add Educator Role
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
+educatorRouter.get('/courses', protectEducator, getEducatorCourses)
 
 export default educatorRouter;
